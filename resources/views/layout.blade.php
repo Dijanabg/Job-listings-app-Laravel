6109ae12a9012fa40dc9009963efd1330e8be296
Dijana Jovanovic <dijanabgjovanovic@gmail.com>
2023-04-12 14:18:52 +0200

    initial + migration, model, factory, seed

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  
+  <title>JobGigs</title>
+</head>
+
+<body class="mb-48">
+  <h1>JobGigs</h1>
+  @yield('content')
+</body>
+
+</html>
